a9abe0602865f1a483f3e3e258efaf4196e53802
William He <william.he@ericsson.com>
2019-01-31 14:34:06 +0800

    Inistal backend code

---
+++ backend/src/main/java/com/hkinron/rentalsystem/backend/rentalsystembackend/RentalsystemBackendApplication.java
@@ -0,0 +1,14 @@
+package com.hkinron.rentalsystem.backend.rentalsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalsystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalsystemBackendApplication.class, args);
+	}
+
+}
+
